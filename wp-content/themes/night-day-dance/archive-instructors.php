d2fcb1eb7605297b6c4ad6d51de86a4236a48485
Levan <user@Macintosh.local>
2016-03-30 14:06:07 -0700

    Fix Sass merge conflicts.

--- wp-content/themes/night-day-dance/archive-instructors.php
+++ wp-content/themes/night-day-dance/archive-instructors.php
@@ -7,54 +7,68 @@
 
 get_header(); ?>
 
-text from archive-instructor.php
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<header class="page-header">
+			<?php
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="taxonomy-description">', '</div>' );
+			?>
+		</header>
 
-<?php
-/*
-* Loop through Categories and Display Posts within
-*/
-$post_type = 'instructors';
+		<?php
+		/*
+		* Loop through Categories and Display Posts within
+		*/
+		$post_type = 'instructors';
 
-// Get all the taxonomies for this post type
-$taxonomies = get_object_taxonomies( array('post_type' => $post_type) );
+		// Get all the taxonomies for this post type
+		$taxonomies = get_object_taxonomies( array('post_type' => $post_type) );
 
-foreach( $taxonomies as $taxonomy ) :
+		foreach( $taxonomies as $taxonomy ) :
 
-	// Gets every "category" (term) in this taxonomy to get the respective posts
-	$terms = get_terms( $taxonomy );
+			// Gets every "category" (term) in this taxonomy to get the respective posts
+			$terms = get_terms( $taxonomy );
 
-	foreach( $terms as $term ) : ?>
+			foreach( $terms as $term ) : ?>
 
-	<p><?php echo $term->name; ?></p>
+			<div class="instructor-location"><?php echo $term->name; ?></div>
+			<div class="location-group">
+				<?php
+				$args = array(
+					'post_type' => $post_type,
+					'posts_per_page' => -1,  //show all posts
+					'hide_empty' => 0,
+					'post_status' => 'publish',
+					'order' => 'DESC',
+					'tax_query' => array(
+						array(
+							'taxonomy' => $taxonomy,
+							'field' => 'slug',
+							'terms' => $term->slug,
+						)));
 
-	<?php
-	$args = array(
-		'post_type' => $post_type,
-		'posts_per_page' => -1,  //show all posts
-		'hide_empty' => 0,
-		'post_status' => 'publish',
-		'order' => 'DESC',
-		'tax_query' => array(
-			array(
-				'taxonomy' => $taxonomy,
-				'field' => 'slug',
-				'terms' => $term->slug,
-	)));
+						$posts = new WP_Query($args);
 
-	$posts = new WP_Query($args);
+						if( $posts->have_posts() ): while( $posts->have_posts() ) : $posts->the_post(); ?>
 
-	if( $posts->have_posts() ): while( $posts->have_posts() ) : $posts->the_post(); ?>
+						<div class="instructor-single">
+							<a href="<?php the_permalink(); ?>">
+								<div>
+									<?php if(has_post_thumbnail()) { ?>
+										<div class="instructor-pic"><?php the_post_thumbnail( 'large' ); ?></div>
+										<?php } ?>
+											<div class="instructor-name">
+												<?php echo get_the_title(); ?>
+											</div>
+									</div>
+							</div>
+						<?php endwhile; endif; ?>
+					<?php endforeach; ?>
+					</div>
+				<?php endforeach; ?>
 
-	<?php if(has_post_thumbnail()) { ?>
-		<div><?php the_post_thumbnail(); ?></div>
-		<?php } ?>
-
-			<?php  echo get_the_title(); ?>
-
-		<?php endwhile; endif; ?>
-
-	<?php endforeach;
-
-endforeach; ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 	<?php get_footer(); ?>
