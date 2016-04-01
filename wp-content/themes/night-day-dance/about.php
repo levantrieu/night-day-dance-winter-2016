e660b89ef979d769a6a0bd5fe4cede057e16ea97
ardalan <ardalanmemar@gmail.com>
2016-04-01 16:03:41 -0700

    fixed about page image

--- wp-content/themes/night-day-dance/about.php
+++ wp-content/themes/night-day-dance/about.php
@@ -10,26 +10,33 @@ get_header(); ?>
   <main id="main" class="site-main" role="main">
 
     <div class="container">
-      <div class="about-page"> About us</div>
-    <section class="helping">
-      <p>Helping Every Day</p>
-      <p>People Dance For <br> Life</p>
-    </section>
-    <div class="who-we-are">Who We Are</div>
-    <section class="about-content">
-      <p class="mobile-ac">Collectively, The instructors of Night and Day Dance have been dancing, performing and teaching for over 60 years!</p>
-      <p class="desktop-ac">Collectively, The instructors of Night and Day Dance have been dancing, performing and teaching for over 60 years! Individually, We offer a wide range of skills and backgrounds. Common to us all is the ability to teach absolutely anyone how to dance!</p>
-    </section>
-    <div class="about-location">Location</div>
-    <section class="location">
-      <p>You can find us around BC and even in Alberta - in Vancouver, Victoria, NewWest, the Sunshine Coast, and Edmonton!</p>
-      <p class="desktop-location">You can find us around BC and even in Alberta - in Vancouver, Victoria, NewWest, the Sunshine Coast, and Edmonton! Check out the maps of our locations below. We offer most private lessons in the privacy of our home studies.</p>
 
-    </section>
-    <div class="about-buttons">
-      <button class="about-book" type="submit">book</button>
-      <button class="about-contact" type="submit">contact us</button>
-    </div>
+      <section class="helping">
+        <!-- <div class="about-shadow"> -->
+        <img class="about-image" src="<?php echo get_template_directory_uri() . '/images/People-dancing' ?>.png" alt=""/>
+      <!-- </div> -->
+        <div class="about-p-wrapper">
+          <p>Helping Every Day</p>
+          <p class="second-line">People Dance For Life</p>
+        </div>
+      </section>
+      <div class="who-we-are">Who We Are</div>
+      <section class="about-content">
+        <p class="mobile-ac">Collectively, The instructors of Night and Day Dance have been dancing, performing and teaching for over 60 years!</p>
+        <p class="desktop-ac">Collectively, The instructors of Night and Day Dance have been dancing, performing and teaching for over 60 years! Individually, We offer a wide range of skills and backgrounds. Common to us all is the ability to teach absolutely anyone how to dance!</p>
+        <button class="ab-desktop" type="submit">book</button>
+      </section>
+      <div class="about-location">Location</div>
+      <section class="location">
+        <p>You can find us around BC and even in Alberta - in Vancouver, Victoria, NewWest, the Sunshine Coast, and Edmonton!</p>
+        <p class="desktop-location">You can find us around BC and even in Alberta - in Vancouver, Victoria, NewWest, the Sunshine Coast, and Edmonton! Check out the maps of our locations below. We offer most private lessons in the privacy of our home studies.</p>
+        <button class="ac-desktop" type="submit">contact us</button>
+
+      </section>
+      <div class="about-buttons">
+        <button class="about-book" type="submit">book</button>
+        <button class="about-contact" type="submit">contact us</button>
+      </div>
 
 
 
--- wp-content/themes/night-day-dance/footer.php
+++ wp-content/themes/night-day-dance/footer.php
@@ -10,38 +10,38 @@
 </div><!-- #content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-<div class="site-info">
+  <div class="site-info">
 
-<div class="footer-form">
-<div class="form-wrapper">
-<div class="wrap">
-<div class="topic-wrapper">
-<span>Night & Day Newsletter</span>
-</div>
-<div class="first-last-wrapper">
-<input type="text" placeholder="first name" class="name">
-<input type="text" placeholder="last name" class="name">
-</div>
-<input type="text" placeholder="email address" class="email">
-<div class=button-wrapper>
-<button class="sign-up" type="submit">sign up</button>
-</div>
-</div>
-</div>
-<div class="icon-wrapper">
-<div class="icons"><i class="fa fa-facebook fa-lg"></i></div>
-<div class="icons"><i class="fa fa-twitter fa-lg"></i></div>
-<div class="icons"><i class="fa fa-instagram fa-lg"></i></div>
-</div>
-<div class="info-wrapper">
-<a class="footer-links" href="<?php echo esc_url( home_url() ); ?>/About/">About</a>
-<a class="footer-links" href="<?php echo esc_url( home_url() ); ?>/About/">contact</a>
-<a class="footer-links" href="<?php echo esc_url( home_url() ); ?>/About/">Outreach Programs</a>
-<a class="footer-links" href="<?php echo esc_url( home_url() ); ?>/About/">FAQ</a>
-</div>
-</div>
+    <div class="footer-form">
+      <div class="form-wrapper">
+        <div class="wrap">
+          <div class="topic-wrapper">
+            <span>Night & Day Newsletter</span>
+          </div>
+          <div class="first-last-wrapper">
+            <input type="text" placeholder="first name" class="name">
+            <input type="text" placeholder="last name" class="name">
+          </div>
+          <input type="text" placeholder="email address" class="email">
+          <div class=button-wrapper>
+            <button class="sign-up" type="submit">sign up</button>
+          </div>
+        </div>
+      </div>
+      <div class="icon-wrapper">
+        <div class="icons"><i class="fa fa-facebook fa-lg"></i></div>
+        <div class="icons"><i class="fa fa-twitter fa-lg"></i></div>
+        <div class="icons"><i class="fa fa-instagram fa-lg"></i></div>
+      </div>
+      <div class="info-wrapper">
+        <a class="footer-links" href="<?php echo esc_url( home_url() ); ?>/About/">About</a>
+        <a class="footer-links" href="<?php echo esc_url( home_url() ); ?>/About/">contact</a>
+        <a class="footer-links" href="<?php echo esc_url( home_url() ); ?>/About/">Outreach Programs</a>
+        <a class="footer-links" href="<?php echo esc_url( home_url() ); ?>/About/">FAQ</a>
+      </div>
+    </div>
 
-</div><!-- .site-info -->
+  </div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
