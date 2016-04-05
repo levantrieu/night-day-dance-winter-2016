126363acc7d39fba7fbeca9fc2d629c93f529e14
ardalan <ardalanmemar@gmail.com>
2016-04-05 10:01:09 -0700

    made a menu for the footer and styled it

--- wp-content/themes/night-day-dance/footer.php
+++ wp-content/themes/night-day-dance/footer.php
@@ -13,47 +13,35 @@
   <div class="site-info">
 
 
-<div class="footer-form">
-<div class="form-wrapper">
-<div class="wrap">
-<div class="topic-wrapper">
-<span>Night & Day Newsletter</span>
-</div>
-<div class="first-last-wrapper">
-<input type="text" placeholder="first name" class="name">
-<input type="text" placeholder="last name" class="name">
-</div>
-<input type="text" placeholder="email address" class="email">
-<div class=button-wrapper>
-<button class="sign-up" type="submit">sign up</button>
-</div>
-</div>
-</div>
-<div class="icon-wrapper">
-<div class="icons"><i class="fa fa-facebook fa-lg"></i></div>
-<div class="icons"><i class="fa fa-twitter fa-lg"></i></div>
-<div class="icons"><i class="fa fa-instagram fa-lg"></i></div>
-</div>
-<div class="info-wrapper">
-  <?php wp_nav_menu( array(
-      'sort_column' => 'menu_order',
-      'theme_location' => 'footer-menu',
-      'menu_class' => 'css-footer-menu',
-  ) ); ?>
-</div>
-</div>
-
-
-    
+    <div class="footer-form">
+      <div class="form-wrapper">
+        <div class="wrap">
+          <div class="topic-wrapper">
+            <span>Night & Day Newsletter</span>
+          </div>
+          <div class="first-last-wrapper">
+            <input type="text" placeholder="first name" class="name">
+            <input type="text" placeholder="last name" class="name">
+          </div>
+          <input type="text" placeholder="email address" class="email">
+          <div class=button-wrapper>
+            <button class="sign-up" type="submit">sign up</button>
+          </div>
+        </div>
+      </div>
       <div class="icon-wrapper">
         <div class="icons"><i class="fa fa-facebook fa-lg"></i></div>
         <div class="icons"><i class="fa fa-twitter fa-lg"></i></div>
         <div class="icons"><i class="fa fa-instagram fa-lg"></i></div>
       </div>
-
+      <div class="info-wrapper">
+        <?php wp_nav_menu( array(
+          'sort_column' => 'menu_order',
+          'theme_location' => 'footer-menu',
+          'menu_class' => 'css-footer-menu',
+        ) ); ?>
+      </div>
     </div>
-
-
   </div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
