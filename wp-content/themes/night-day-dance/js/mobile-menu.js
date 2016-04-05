99bf7e60fa04b516d14e53600bd81fc359815792
John Peacock <johnpeacock@Johns-MacBook-Pro.local>
2016-04-05 13:12:59 -0700

    fix enqueue for mobile menu and jquery script

--- wp-content/themes/night-day-dance/js/mobile-menu.js
+++ wp-content/themes/night-day-dance/js/mobile-menu.js
@@ -1,26 +1,29 @@
-$(function() {
-	// Insert Responsive Navigation Icon, Close Icon, and Overlay
-	// If you have access to your HTML, you should put this directly into your markup.
-	$('<div class="responsive-nav-icon" />').appendTo('.site-content');
-	$('<div class="responsive-nav-close" />').appendTo('nav');
-	$('<div id="overlay" />').insertAfter('footer');
 
-	// Navigation Slide In
-	$('.responsive-nav-icon').click(function() {
-		$('nav').addClass('slide-in');
-		$('html').css("overflow", "hidden");
-		$('#overlay').show();
-		return false;
-	});
+(function($) {
+	$(function() {
+		// Insert Responsive Navigation Icon, Close Icon, and Overlay
+		// If you have access to your HTML, you should put this directly into your markup.
+		$('<div class="responsive-nav-icon" />').appendTo('.site-content');
+		$('<div class="responsive-nav-close" />').appendTo('nav');
+		$('<div id="overlay" />').insertAfter('footer');
 
-	// Navigation Slide Out
-	$('#overlay, .responsive-nav-close').click(function() {
-		$('nav').removeClass('slide-in');
-		$('html').css("overflow", "auto");
-		$('#overlay').hide();
-		return false;
+		// Navigation Slide In
+		$('.responsive-nav-icon').click(function() {
+			$('nav').addClass('slide-in');
+			$('html').css("overflow", "hidden");
+			$('#overlay').show();
+			return false;
+		});
+
+		// Navigation Slide Out
+		$('#overlay, .responsive-nav-close').click(function() {
+			$('nav').removeClass('slide-in');
+			$('html').css("overflow", "auto");
+			$('#overlay').hide();
+			return false;
+		});
 	});
-});
+})( jQuery );
 
 // sub-menu code //////////////////// sub-menu code /////////////////
 
